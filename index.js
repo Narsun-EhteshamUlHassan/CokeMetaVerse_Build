ce00da1a5cb1ade78a56910a24fa5d00db3745d9
Narsun-EhteshamUlHassan <ehteshamulhassan@narsunstudios.com>
2026-03-26 12:59:51 +0500

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+window.setUnityInstance = function (instance) {
+	window.XTND = {
+		unity: instance,
+		isReady: false,
+		cachedUser: null
+	};
+	initXTND();
+}
+
+async function initXTND() {
+	try {
+
+		if (!window.xtnd) {
+			console.warn("XTND SDK not loaded yet");
+			return;
+		}
+
+		window.XTND.isReady = true;
+
+	} catch (e) {
+		console.error("XTND init error", e);
+	}
+}
